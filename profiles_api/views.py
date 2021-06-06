685c1afa4cd94aa56b0860ea066a652f82839bd1
Muneeb <tmuneebanjum@gmail.com>
2021-06-06 07:34:58 +0500

    Creating API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+frpm rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+
+    def get(self,request, format=None):
+        
+        an_apiview = [
+            
+            'User HTTp methods as function (get, post, patch, put, delete)',
+            'Is mapped manually to URLs',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            
+            
+        ]
+        
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+    
+        
