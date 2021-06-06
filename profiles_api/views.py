1e4ba06adf57c10088fa95d26504b677c2a098eb
Muneeb <tmuneebanjum@gmail.com>
2021-06-06 13:51:00 +0500

    Post Method created

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import Status
+from rest_framework import status
 
 from profiles_api import serializers
 
